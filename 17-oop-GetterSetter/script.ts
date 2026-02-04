97c6f6a3ea46d5519eb772ed9daae077b38262d8
Samar85-Y <samar85yousef@gmail.com>
2026-02-04 10:52:44 +0100

    feat: chapter 17 finished

---
+++ 17-oop-GetterSetter/script.ts
@@ -0,0 +1,54 @@
+class Point2D {
+  private _xPos: number; // ohne weiter Angaben sind die Properties immer Public
+  private _yPos: number;
+
+  constructor(xPosInput: number, yPosInput: number) {
+    this._xPos = xPosInput;
+    this._yPos = yPosInput;
+  }
+
+  public get xPos(): number {
+    return this._xPos;
+  }
+  public get yPos(): number {
+    return this._yPos;
+  }
+
+  public set xPos(xPosNew: number) {
+    this._xPos = xPosNew;
+  }
+  public set yPos(yPosNew: number) {
+    this._xPos = yPosNew;
+  }
+}
+
+
+class Point3D extends Point2D {
+  private _zPos: number;
+
+  constructor(xPosInput: number, yPosInput: number, zPostInput: number) {
+    super(xPosInput, yPosInput);
+    this._zPos = zPostInput;
+  }
+
+  public get zPos(): number {
+    return this._zPos;
+  }
+
+  public set zPos(zPosNew: number) {
+    this._zPos = zPosNew;
+  }
+}
+
+const p1: Point2D = new Point2D(0, 0);
+const p2: Point3D = new Point3D(1, 1, 5);
+
+console.log(p1, p2);
+
+
+// Getter-/Setter-Methoden werden ohne runde Klammen aufgrufen
+//Aufruf der xPos-Methode mit Wertzuweisung = Setter-Methode
+p1.xPos = 4; //setter methode einrufe
+
+//Aufruf der xPos-Methode ohne Wertzuweisung = Getter-Methode
+console.log(p1.xPos, p2);
